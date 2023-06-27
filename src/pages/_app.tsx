41d6c0e7e8708cea4510aeb154f5e2f822562db4
kangkong <kang3530554@gmail.com>
2023-06-27 09:34:12 +0900

    📦 Update: GlobalStyle import문 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals'
+import GlobalStyle from '@/styles/globals'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }

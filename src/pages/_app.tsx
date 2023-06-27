0e30f87aad9a9f92dd234e6371844546f101cfcc
kangkong <kang3530554@gmail.com>
2023-06-27 14:18:08 +0900

    ➕ Add : recoil, toastify 라이브러리 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import GlobalStyle from '@/styles/globals'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+      <ToastContainer />
+    </RecoilRoot>
   )
 }
